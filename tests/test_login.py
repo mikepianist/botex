844b648baf7a81057aa04e08eccee92a61066cfc
mikepianist <mikehove@hotmail.com>
2024-10-04 16:49:33 -0300

    Mejorando sintaxis

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,10 @@
-# test_login.py
-from dotenv import load_dotenv
+"""
+Este módulo contiene una prueba para verificar la funcionalidad de login en Botex.
+Obtiene las credenciales de un archivo .env y las imprime para realizar el test.
+"""
+
 import os
+from dotenv import load_dotenv
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
@@ -12,6 +16,10 @@ password = os.getenv("PASSWORD")
 
 # Aquí podrías utilizar las variables para la lógica de login
 def test_login():
+    """
+    Función de prueba para simular un login.
+    Obtiene las credenciales desde el archivo .env y las imprime.
+    """
     print(f"Email: {email}")
     print(f"Password: {password}")
     # Lógica para iniciar sesión con email y contraseña...
